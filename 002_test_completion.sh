e4f91d302fcbc4405e9b6cb17ac4e9051bb62fe9
Shawn Presser <shawnpresser@gmail.com>
2020-09-08 20:42:48 -0700

    Initial completion server

--- 002_test_completion.sh
+++ 002_test_completion.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -ex
+engine="${1:-davinci}"
+prompt="${2:-Test}"
+temp="${3:-0.6}"
+max_tokens="${4:-12}"
+n="${5:-1}"
+set +e
+shift 1
+shift 1
+shift 1
+shift 1
+shift 1
 export OPENAI_API_KEY="${OPENAI_API_KEY:-stub}"
 export OPENAI_API_BASE="${OPENAI_API_BASE:-http://localhost:8000}"
 export OPENAI_LOG="${OPENAI_LOG:-debug}"
-exec openai api completions.create -e davinci -p 'Test' -M 12 -t 0.6 -n 1 "$@"
+exec openai api completions.create -e "${engine}" -p "${prompt}" -M "${max_tokens}" -t "${temp}" -n "${n}" "$@"
--- openai_server/__init__.py
+++ openai_server/__init__.py
@@ -1,9 +1,112 @@
 import os
 import sys
+import tqdm
+import traceback
+import time
+import base64
+import secrets
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'gpt'))
 from json import loads
+from json import load as json_load
 from sanic import Sanic
 from sanic.response import json, text
 from pprint import pprint as pp
+
+from openai_server.gpt import sample, model, encoder
+
+import tensorflow as tf
+
+
+class GPTEngine:
+  def __init__(self, api, model_name, batch_size=1):
+    self.api = api
+    self.id = model_name
+    self.ckpt = tf.train.latest_checkpoint(os.path.join(api.model_path, model_name))
+    if self.ckpt is None:
+      raise ValueError("Couldn't load checkpoint for {model_name} from {path}".format(model_name=model_name, path=os.path.join(api.model_path, model_name)))
+    self.graph = tf.Graph()
+    self.session = tf.Session(graph=self.graph)
+    self.encoder = encoder.get_encoder(model_name, self.api.model_path)
+    self.hparams = model.default_hparams()
+    with open(os.path.join(self.api.model_path, model_name, 'hparams.json')) as f:
+      self.hparams.override_from_dict(json_load(f))
+    with self.session.as_default() as sess, self.graph.as_default() as graph:
+      self.batch_size = batch_size
+      self.context = tf.placeholder(tf.int32, [self.batch_size, None], name="context")
+      self.length = tf.placeholder(tf.int32, (), name="length")
+      self.temperature = tf.placeholder(tf.float32, (), name="temperature")
+      self.top_k = tf.placeholder(tf.int32, (), name="top_k")
+      self.top_p = tf.placeholder(tf.float32, (), name="top_p")
+      #np.random.seed(seed)
+      #tf.set_random_seed(seed)
+      self.output = sample.sample_sequence(
+          hparams=self.hparams, length=self.length,
+          context=self.context,
+          batch_size=self.batch_size,
+          temperature=self.temperature, top_k=self.top_k, top_p=self.top_p
+      )
+      
+      self.saver = tf.train.Saver(var_list=tf.trainable_variables())
+      self.saver.restore(sess, self.ckpt)
+
+  def completion(self, prompt, n=None, max_tokens=None, logprobs=None, stream=False, temperature=None, top_p=None, top_k=None, echo=None):
+    if temperature is None:
+      temperature = 0.9
+    if top_p is None:
+      top_p = 1.0
+    if top_k is None:
+      top_k = 0
+    if max_tokens is None:
+      max_tokens = 16
+    if n is None:
+      n = 1
+    if echo is None:
+      echo = True
+    with self.session.as_default() as sess, self.graph.as_default() as graph:
+      tokens = self.encoder.encode(prompt)
+      length = len(tokens) + max_tokens
+      result = self.session.run(self.output, {
+        self.context: [tokens],
+        self.temperature: temperature,
+        self.top_p: top_p,
+        self.top_k: top_k,
+        self.length: length,
+      })
+      text = self.encoder.decode(result[0])
+      if not echo:
+        assert text.startswith(prompt)
+        text = text[len(prompt):]
+      print(repr(text))
+      yield text
+
+class API:
+  def __init__(self, model_path=None):
+    if model_path is None:
+      model_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'models')
+    self.model_path = model_path
+    self.models = []
+    self.engines = {}
+    for model in tqdm.tqdm(os.listdir(self.model_path)):
+      try:
+        engine = GPTEngine(api=self, model_name=model)
+        self.engines[model] = engine
+        self.models.append(model)
+      except:
+        traceback.print_exc()
+    pp(self.engines)
+    pp(self.models)
+
+
+  def engines_list(self):
+    for model in self.models:
+      yield {
+          "id": model,
+          "object": "engine",
+          "owner": "openai",
+          "ready": True,
+      }
+
+api = API()
 
 app = Sanic()
 
@@ -25,6 +128,10 @@ def log_request(request):
 @app.route('/v1/engines')
 async def v1_engines_list(request):
   log_request(request)
+  res = {"object": "list", "data": []}
+  for result in api.engines_list():
+    res["data"].append(result)
+  return json(res)
   return json({
     "data": [
       {
@@ -79,10 +186,24 @@ async def v1_engines_list(request):
     "object": "list"
   })
 
-@app.route('/v1/engines/davinci/completions', methods=['POST'])
-async def v1_engines_davinci_completions(request):
+
+def random_id(prefix, nbytes=18):
+  token = secrets.token_bytes(nbytes)
+  return prefix + '-' + base64.urlsafe_b64encode(token).decode('utf8')
+
+
+@app.route('/v1/engines/<engine_name>/completions', methods=['POST'])
+async def v1_engines_davinci_completions(request, engine_name):
   log_request(request)
-  return json({"id": "cmpl-Wt5z1RZglyDHHl0SnSvKWVzA", "object": "text_completion", "created": 1599616871, "model": "davinci:2020-05-03", "choices": [{"text": "Test.SetLayerPropertiesWithNonContainedInvisible (", "index": 0, "logprobs": None, "finish_reason": "length"}]})
+  kws = request.json
+  pp(kws)
+  engine = api.engines[engine_name]
+  choices = []
+  for index, text in enumerate(engine.completion(**kws)):
+    choices.append({'text': text, 'index': index, 'logprobs': None, 'finish-reason': 'length'})
+  id_ = random_id("cmpl")
+  return json({"id": id_, "object": "text_completion", "created": time.time(), "model": engine.id, "choices": choices})
+  #return json({"id": "cmpl-Wt5z1RZglyDHHl0SnSvKWVzA", "object": "text_completion", "created": 1599616871, "model": "davinci:2020-05-03", "choices": [{"text": "Test.SetLayerPropertiesWithNonContainedInvisible (", "index": 0, "logprobs": None, "finish_reason": "length"}]})
     
 if __name__ == '__main__':
   args = sys.argv[1:]
