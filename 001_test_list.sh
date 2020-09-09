6e9f4eab7c27f803a9ff52afb9365258e9723389
Shawn Presser <shawnpresser@gmail.com>
2020-09-08 19:33:24 -0700

    Update

--- 001_test_list.sh
+++ 001_test_list.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -ex
 export OPENAI_API_KEY="${OPENAI_API_KEY:-stub}"
-OPENAI_API_BASE='http://localhost:8000' OPENAI_LOG=debug exec openai api engines.list "$@"
+export OPENAI_API_BASE="${OPENAI_API_BASE:-http://localhost:8000}"
+export OPENAI_LOG="${OPENAI_LOG:-debug}"
+exec openai api engines.list "$@"
--- 002_test_completion.sh
+++ 002_test_completion.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -ex
 export OPENAI_API_KEY="${OPENAI_API_KEY:-stub}"
-OPENAI_API_BASE='http://localhost:8000' OPENAI_LOG=debug exec openai api completions.create -e davinci -p 'Test' -M 12 -t 0.6 -n 1 "$@"
+export OPENAI_API_BASE="${OPENAI_API_BASE:-http://localhost:8000}"
+export OPENAI_LOG="${OPENAI_LOG:-debug}"
+exec openai api completions.create -e davinci -p 'Test' -M 12 -t 0.6 -n 1 "$@"
