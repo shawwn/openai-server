c8a5947da8dd95e31db256526b54c33400532d3a
Shawn Presser <shawnpresser@gmail.com>
2020-10-02 22:30:26 -0700

    Update 002_test_completion.sh

--- 002_test_completion.sh
+++ 002_test_completion.sh
@@ -6,17 +6,19 @@ export OPENAI_API_BASE="${OPENAI_API_BASE:-http://localhost:${PORT}}"
 #export OPENAI_API_BASE="${OPENAI_API_BASE:-https://api.openai.com}"
 #export OPENAI_LOG="${OPENAI_LOG:-debug}"
 
-engine="${1:-davinci}"
-prompt="${2:-Test}"
-temp="${3:-0.6}"
-max_tokens="${4:-12}"
-n="${5:-4}"
+prompt="${1:-Hello, my name is}"
+
+if [ -e "$prompt" ]
+then
+  prompt="$(cat "$prompt")"
+fi
+
+ENGINE="${ENGINE:-${E:-davinci}}"
+TEMPERATURE="${TEMPERATURE:-${T:-0.9}}"
+MAX_TOKENS="${MAX_TOKENS:-${M:-12}}"
+N="${N:-1}"
 set +ex
-shift 1
-shift 1
-shift 1
-shift 1
 shift 1
 
 set -x
-exec openai api completions.create -e "${engine}" -p "${prompt}" -t "${temp}" -M "${max_tokens}" -n "${n}" "$@"
+exec openai api completions.create -e "${ENGINE}" -t "${TEMPERATURE}" -M "${MAX_TOKENS}" -n "${N}" -p "${prompt}" "$@"
